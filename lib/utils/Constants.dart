227c5261ce6ca553a1ff8107bdc0b4289a8fc398
jamescasia <jamescasia99@gmail.com>
2022-02-19 16:56:48 +0800

    feat(utils): added constants

---
+++ lib/utils/Constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const fabColor = Color(0xff00B0DF);
+  static const bgColor = Color(0xfff3f3f3);
+  static const noteColor = Color(0xfffffbe5);
+  static const noteColorLight = Color(0xffFFFEF6);
+  static const allColor = Color(0xffFAB212);
+  static const doneColor = Color(0xff47be02);
+  static const notDonecolor = Color(0xff444444);
+}
