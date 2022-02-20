0831439f4fbde1c2bb9862cfb025ea37ad3ba976
jamescasia <jamescasia99@gmail.com>
2022-02-20 22:37:11 +0800

    feat(views): added text validation

--- lib/views/AddTaskModal.dart
+++ lib/views/AddTaskModal.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:toodoo/models/Task.dart';
 
@@ -19,6 +20,8 @@ class _AddTaskModalState extends State<AddTaskModal> {
   final TextEditingController descController = TextEditingController();
   final FocusNode titleFocus = FocusNode();
   final FocusNode descFocus = FocusNode();
+  bool titleError = false;
+  bool descError = false;
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<AppModel>(builder: (context, child, appModel) {
@@ -55,18 +58,36 @@ class _AddTaskModalState extends State<AddTaskModal> {
                       child: Center(
                         child: TextField(
                           controller: titleController,
-                          maxLength: 35,
+                          maxLength: 30,
                           maxLines: 1,
                           focusNode: titleFocus,
                           onSubmitted: (text) {
-                            FocusScope.of(context).requestFocus(descFocus);
+                            if (text.isNotEmpty) {
+                              FocusScope.of(context).requestFocus(descFocus);
+                            } else {
+                              setState(() {
+                                titleError = true;
+
+                                FocusScope.of(context).requestFocus(titleFocus);
+                              });
+                            }
+                          },
+                          onChanged: (text) {
+                            if (text.isNotEmpty) {
+                              setState(() {
+                                titleError = false;
+                              });
+                            }
                           },
                           decoration: InputDecoration.collapsed(
-                              hintText: "Task name...",
+                              hintText:
+                                  titleError ? "Add task name" : "Task name...",
                               hintStyle: TextStyle(
                                   fontFamily: "Manrope",
                                   fontWeight: FontWeight.bold,
-                                  color: Constants.textColor.withOpacity(0.6),
+                                  color: titleError
+                                      ? Constants.delColor.withOpacity(0.8)
+                                      : Constants.textColor.withOpacity(0.6),
                                   fontSize: 18)),
                           style: TextStyle(
                               fontFamily: "Manrope",
@@ -87,8 +108,12 @@ class _AddTaskModalState extends State<AddTaskModal> {
                         alignment: Alignment.topLeft,
                         child: TextField(
                           controller: descController,
-                          maxLength: 100,
+                          maxLength: 70,
                           maxLines: 3,
+                          inputFormatters: [
+                            FilteringTextInputFormatter("\n", allow: false)
+                          ],
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
                           focusNode: descFocus,
                           onSubmitted: (text) {
                             if (titleController.text.isNotEmpty) {
@@ -110,11 +135,15 @@ class _AddTaskModalState extends State<AddTaskModal> {
                             }
                           },
                           decoration: InputDecoration.collapsed(
-                              hintText: "Add task description...",
+                              hintText: descError
+                                  ? "Add Task description"
+                                  : "Task description...",
                               hintStyle: TextStyle(
                                   fontFamily: 'Manrope',
                                   fontSize: 16,
-                                  color: Constants.textColor.withOpacity(0.6),
+                                  color: descError
+                                      ? Constants.delColor.withOpacity(0.8)
+                                      : Constants.textColor.withOpacity(0.6),
                                   fontWeight: FontWeight.w300)),
                           style: TextStyle(
                               fontFamily: 'Manrope',
@@ -144,8 +173,24 @@ class _AddTaskModalState extends State<AddTaskModal> {
                       "assets/icons/check_icon.png",
                     ),
                     onPressed: () {
-                      if (titleController.text.isNotEmpty &&
-                          descController.text.isNotEmpty) {
+                      if (titleController.text.isEmpty &&
+                          descController.text.isEmpty) {
+                        setState(() {
+                          titleError = true;
+                          descError = true;
+                          FocusScope.of(context).requestFocus(titleFocus);
+                        });
+                      } else if (titleController.text.isEmpty) {
+                        setState(() {
+                          titleError = true;
+                          FocusScope.of(context).requestFocus(titleFocus);
+                        });
+                      } else if (descController.text.isEmpty) {
+                        setState(() {
+                          descError = true;
+                          FocusScope.of(context).requestFocus(descFocus);
+                        });
+                      } else {
                         Task task = Task(
                             titleController.text +
                                 Random().nextInt(pow(2, 32).toInt()).toString(),
