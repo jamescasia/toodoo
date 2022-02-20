8c8e03acf87dd72fa9bddf73810d3494db77f454
jamescasia <jamescasia99@gmail.com>
2022-02-20 14:55:25 +0800

    feat(test): added base appstate tests

---
+++ test/tasksbeingviewed_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:toodoo/models/AppModel.dart';
+import 'package:toodoo/models/Task.dart';
+
+void main() {
+  group('Tasks being viewed', () {
+    test('Initial tasks being viewed should be equal to tasks', () {
+      AppModel appModel = AppModel();
+      List<Task> tasks = [
+        Task("1", "Walk the dogs", "walk the dogs and do 5 laps", false, true),
+        Task("2", "Get laundry", "Get laundry at laundryhouse at 3PM", true,
+            true),
+      ];
+      appModel.setTasks(tasks);
+
+      expect(appModel.tasksBeingViewed, tasks);
+    });
+
+    test('Tasks being viewed should be all tasks', () {
+      AppModel appModel = AppModel();
+      List<Task> tasks = [
+        Task("1", "Walk the dogs", "walk the dogs and do 5 laps", false, true),
+        Task("2", "Get laundry", "Get laundry at laundryhouse at 3PM", true,
+            true),
+      ];
+      appModel.setTasks(tasks);
+
+      appModel.viewAllTasks();
+
+      expect(appModel.tasksBeingViewed, tasks);
+    });
+
+    test('Tasks being viewed should be done tasks', () {
+      AppModel appModel = AppModel();
+      List<Task> tasks = [
+        Task("1", "Walk the dogs", "walk the dogs and do 5 laps", false, true),
+        Task("2", "Get laundry", "Get laundry at laundryhouse at 3PM", true,
+            true),
+      ];
+      appModel.setTasks(tasks);
+
+      appModel.viewDoneTasks();
+
+      for (Task task in appModel.tasksBeingViewed) {
+        expect(task.done, true);
+      }
+    });
+
+    test('Tasks being viewed should be not done tasks', () {
+      AppModel appModel = AppModel();
+      List<Task> tasks = [
+        Task("1", "Walk the dogs", "walk the dogs and do 5 laps", false, true),
+        Task("2", "Get laundry", "Get laundry at laundryhouse at 3PM", true,
+            true),
+      ];
+      appModel.setTasks(tasks);
+
+      appModel.viewNotDoneTasks();
+
+      for (Task task in appModel.tasksBeingViewed) {
+        expect(task.done, false);
+      }
+    });
+  });
+}
---
+++ test/viewingstate_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:toodoo/models/AppModel.dart';
+
+void main() {
+  group('Viewing State', () {
+    test('Initial viewing state should be All', () {
+      expect(AppModel().viewingState, ViewingState.All);
+    });
+
+    test('viewing state should be changed to Done', () {
+      final appModel = AppModel();
+
+      appModel.viewDoneTasks();
+
+      expect(appModel.viewingState, ViewingState.Done);
+    });
+
+    test('viewing state should be changed to Not Done', () {
+      final appModel = AppModel();
+
+      appModel.viewNotDoneTasks();
+
+      expect(appModel.viewingState, ViewingState.NotDone);
+    });
+
+    test('viewing state should be changed to All', () {
+      final appModel = AppModel();
+
+      appModel.viewAllTasks();
+
+      expect(appModel.viewingState, ViewingState.All);
+    });
+  });
+}
